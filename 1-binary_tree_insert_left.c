4a756e48e531e1ce41d5bcbb861c5e7e692ac0ab
Bright Olawale <oolatunjibright@yahoo.com>
2022-08-25 12:22:27 +0100

    Add to left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *temp = malloc(sizeof(binary_tree_t));
+    binary_tree_t *leftChild = malloc(sizeof(binary_tree_t));
+
+
+    if (!temp || !parent || !leftChild)
+        return NULL;
+
+    leftChild->n = value;
+	leftChild->left = NULL;
+	leftChild->right = NULL;
+
+
+    if (parent->left == NULL)
+    {
+        parent->left = leftChild;
+        leftChild->parent = parent;
+    }
+    else
+    {
+        temp = parent->left;
+        parent->left = leftChild;
+        leftChild->left = temp;
+        leftChild->parent = parent;
+        leftChild->left->parent = leftChild;
+    }
+
+    return (leftChild);
+}
