c3715d48229903eea4d8978449de9e4194a34503
Bright Olawale <oolatunjibright@yahoo.com>
2022-08-27 07:51:15 +0100

    Finding sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,13 +11,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-	if ((node->parent->left != node) && node->parent->right)
-	{
+	if (node->parent->left == node)
 		return (node->parent->right);
-	}
-	else if ((node->parent->right != node) && node->parent->left)
-	{
-		return (node->parent->left);
-	}
-	return (NULL);
+	return (node->parent->left);
 }
